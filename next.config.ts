26d5f2ebc3382d4adb47c4cf185a4f8524d56ce9
Luqman Ali <luqmanali05@hotmail.com>
2026-01-11 03:53:22 +0500

    503 error fix on hostinger

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   images: {
     remotePatterns: [
       {
@@ -9,9 +9,6 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
       },
     ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
 
